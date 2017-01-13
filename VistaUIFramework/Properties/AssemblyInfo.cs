288b78d9175feb617308013d9c8bf3df2a1bb563
myapkapp <support@myapkapp.com>
2017-01-13 13:02:57 -0400

    First commits

---
+++ VistaUIFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("VistaUIFramework")]
+[assembly: AssemblyDescription("VistaUIFramework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("myapkapp")]
+[assembly: AssemblyProduct("VistaUIFramework")]
+[assembly: AssemblyCopyright("Copyright © myapkapp 2017")]
+[assembly: AssemblyTrademark("myapkapp")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("d2c0b79e-9e39-4d0a-b399-a620b63b1662")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
